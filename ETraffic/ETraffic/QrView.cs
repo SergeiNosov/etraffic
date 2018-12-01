fe44c976ed9c1169a860e5f479e831ed982dcee1
andrey <p9gehka@mail.ru>
2018-12-01 19:48:27 +0300

    Qr work

--- ETraffic/ETraffic/QrView.cs
+++ ETraffic/ETraffic/QrView.cs
@@ -2,6 +2,7 @@ using Foundation;
 using System;
 using UIKit;
 using CoreGraphics;
+using Xamarin.Forms;
 
 namespace ETraffic
 {
@@ -17,14 +18,13 @@ namespace ETraffic
         CGPath path;
         CGPoint initialPoint;
         CGPoint latestPoint;
-
-
+        QRCodeData qrCodeData;
         public QrView(IntPtr handle) : base(handle)
         {
             BackgroundColor = UIColor.White;
             path = new CGPath();
             QRGenerator qrGenerator = new QRGenerator();
-            Console.WriteLine(qrGenerator.StringToQRBin("HELLO"));
+            qrCodeData = qrGenerator.CreateQrCode("HELLO", QRGenerator.ECCLevel.M);
         }
 
         public override void TouchesBegan(NSSet touches, UIEvent evt)
@@ -64,27 +64,35 @@ namespace ETraffic
                 //get graphics context
                 using (CGContext g = UIGraphics.GetCurrentContext())
                 {
-                  
-                    //set up drawing attributes
-                    g.SetLineWidth(2);
-                    UIColor.Red.SetStroke();
-
-                    //add lines to the touch points
-                    if (path.IsEmpty)
+                    var drawQuietZones = true;
+                    var pixelsPerModule = 10;
+                    var size = (this.qrCodeData.ModuleMatrix.Count - (drawQuietZones ? 0 : 8)) * pixelsPerModule;
+                    var offset = drawQuietZones ? 0 : 4 * pixelsPerModule;
+                    Console.WriteLine(String.Format("Size: {0}", size));
+                    Console.WriteLine(String.Format("Offset: {0}", offset));
+                    for (var x = 0; x < size + offset; x = x + pixelsPerModule)
                     {
-                        path.AddLines(new CGPoint[] { initialPoint, latestPoint });
-                    }
-                    else
-                    {
-                        path.AddLineToPoint(latestPoint);
-                    }
+                        for (var y = 0; y < size + offset; y = y + pixelsPerModule)
+                        {
+                            var module = this.qrCodeData.ModuleMatrix[(y + pixelsPerModule) / pixelsPerModule - 1][(x + pixelsPerModule) / pixelsPerModule - 1];
+                            if (module)
+                            {
+                                UIColor.Black.SetFill();
+                                Console.WriteLine(String.Format("({0},{1}) {2}", x-offset, y-offset, "Green"));
+                                g.AddRect(new CGRect(x - offset, y - offset, pixelsPerModule, pixelsPerModule));
+                            }
+                            else
+                            {
+                                /*UIColor.Red.SetFill();*/
+                                Console.WriteLine(String.Format("({0},{1}) {2}", x-offset, y-offset, "Red"));
 
-                    //use a dashed line
-                    g.SetLineDash(0, new nfloat[] { 5, 2 * (nfloat)Math.PI });
-
-                    //add geometry to graphics context and draw it
-                    g.AddPath(path);
-                    g.DrawPath(CGPathDrawingMode.Stroke);
+                            }
+                            Console.WriteLine(String.Format("PixelsPerModule: {0}", pixelsPerModule));
+                           
+                        }
+                    }
+                    g.DrawPath(CGPathDrawingMode.Fill);
+                    Console.WriteLine("Redraw");
                 }
             }
         }
